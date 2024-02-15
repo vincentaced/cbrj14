f2e9a7d78205e29be2fcd425cf729894e4d4388a
vincentaced <159973253+vincentaced@users.noreply.github.com>
2024-02-15 17:07:31 +0800

    Initial commit

---
+++ resources/views/app/admin/index.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'CBRJ\'s Admin')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('footer')
+Copyright &copy 2024, CBRJ IS. All rights reserved
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
